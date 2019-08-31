a6538ffac144eac2dd64bbd4d811eb82a87decaa
unknown <2149194@labinfo.is.escuelaing.edu.co>
2019-08-31 07:35:14 -0500

    Especificacion metodos

---
+++ src/main/java/hangman/model/OriginalScore.java
@@ -0,0 +1,17 @@
+package hangman.model;
+
+public class OriginalScore implements GameScore{
+
+	@Override
+	/**
+	 * @pre se inicia el puntaje con 100 puntos  
+	 * @pos el puntaje minimo es 0
+	 * @param correctCount letras correctas
+	 * @param incorrectCount letras incorrectas
+	 */
+	public int calculateScore(int correctCount, int incorrectCount) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
